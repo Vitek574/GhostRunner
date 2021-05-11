8f8d18eb9ef52538825aa9def195c41b63064c42
Vítek574 <kulatyvitek@seznam.cz>
2021-05-11 11:38:45 +0200

    Přidaná výhra

--- src/main/kotlin/cz/vitek574/ghostrunner/blocks/Hero.kt
+++ src/main/kotlin/cz/vitek574/ghostrunner/blocks/Hero.kt
@@ -16,19 +16,27 @@ class Hero(override val position: Position) : Block {
             0 -> if (neighbors.bottom is Whitespace) position.posY += 1
             else if (neighbors.bottom is GhostHorizontal)
                 println("hrdina přišel o život")
+            else if (neighbors.bottom is Finish)
+                println("gratuluji vyhrál jsi")
             else println("tak nic já nemůžu jít")
             1 -> if (neighbors.left is Whitespace) position.posX -= 1
             else if (neighbors.left is GhostHorizontal)
                 println("hrdina přišel o život")
+            else if (neighbors.left is Finish)
+                println("gratuluji vyhrál jsi")
             else println("tak nic já nemůžu jít")
             2 -> if (neighbors.right is Whitespace) position.posX += 1
             else if (neighbors.right is GhostHorizontal)
                 println("hrdina přišel o život")
+            else if (neighbors.right is Finish)
+                println("gratuluji vyhrál jsi")
             else println("tak nic já nemůžu jít")
             3 -> if (neighbors.top is Whitespace) position.posY -= 1
             else if (neighbors.top is GhostHorizontal)
+                println("hrdina přišel o život")
+            else if (neighbors.top is Finish)
+                println("gratuluji vyhrál jsi")
             else println("tak nic já nemůžu jít")
         }
     }
-
 }
